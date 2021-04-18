5ea0c5faa57667d5a57d08a4a5d89e5d195b257e
w00j00ng <w00j00ng351@gmail.com>
2021-04-19 00:43:24 +0900

    End printfAndScanf

--- printfscanf.c
+++ printfscanf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main_printfscanf(void)
 {
 	//// 정수형 변수에 대한 예제
 	//int age = 12;
@@ -55,33 +55,33 @@ int main(void)
 	// 경찰관이 범죄자의 정부를 입수 (조서 작성)
 	// 이름? 나이? 몸무게? 키? 범죄명
 
-	char name[256];
-	printf("이름이 뭐예요? ");
-	scanf_s("%s", name, sizeof(name));
+	//char name[256];
+	//printf("이름이 뭐예요? ");
+	//scanf_s("%s", name, sizeof(name));
 
-	int age;
-	printf("몇 살이예요? ");
-	scanf_s("%d", &age);
+	//int age;
+	//printf("몇 살이예요? ");
+	//scanf_s("%d", &age);
 
-	float weight;
-	printf("몸무게는 몇 kg이예요? ");
-	scanf_s("%f", &weight);
+	//float weight;
+	//printf("몸무게는 몇 kg이예요? ");
+	//scanf_s("%f", &weight);
 
-	double height;
-	printf("키는 몇 cm이예요? ");
-	scanf_s("%lf", &height);
+	//double height;
+	//printf("키는 몇 cm이예요? ");
+	//scanf_s("%lf", &height);
 
-	char what[256];
-	printf("무슨 범죄를 저질렀어요? ");
-	scanf_s("%s", what, sizeof(what));
+	//char what[256];
+	//printf("무슨 범죄를 저질렀어요? ");
+	//scanf_s("%s", what, sizeof(what));
 
-	// 조서 내용 출력
-	printf("\n\n--- 범죄자 정보 ---\n\n");
-	printf(" 이름	: %s\n", name);
-	printf(" 나이	: %d\n", age);
-	printf(" 몸무게	: %.2f\n", weight);
-	printf(" 키	: %.2lf\n", height);
-	printf(" 범죄	: %s\n", what);
+	//// 조서 내용 출력
+	//printf("\n\n--- 범죄자 정보 ---\n\n");
+	//printf(" 이름	: %s\n", name);
+	//printf(" 나이	: %d\n", age);
+	//printf(" 몸무게	: %.2f\n", weight);
+	//printf(" 키	: %.2lf\n", height);
+	//printf(" 범죄	: %s\n", what);
 
 	return 0;
 }
