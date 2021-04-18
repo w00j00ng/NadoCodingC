0d991749bc7a79163a4e845b9194268395aed4d4
w00j00ng <w00j00ng351@gmail.com>
2021-04-19 01:18:23 +0900

    UpAndDown 1

--- condition.c
+++ condition.c
@@ -126,5 +126,41 @@ int main(void)
 	}*/
 
 	// Up and Down
+	srand(time(NULL));
+	int num = rand() % 100 + 1; // 1 ~ 100 사이의 임의 숫자
+	printf("숫자 : %d\n", num);
+	
+	int answer = 0; // 정답
+	int chance = 5; // 기회
+	while (1) // 1: 참, 0: 거짓
+	{
+		printf("남은 기회 %d번\n", chance--);
+		printf("숫자를 맞춰보세요 (1~100): ");
+		scanf_s("%d", &answer);
+
+		if (chance == 0)
+		{
+			printf("모든 기회를 다 사용하셨네요, 아쉽게 패배하였습니다.\n\n");
+			break;
+		}
+
+		if (answer > num)
+		{
+			printf("DOWN ↓\n\n");
+		}
+		else if (answer < num)
+		{
+			printf("UP ↑\n\n");
+		}
+		else if (answer == num)
+		{
+			printf("정답입니다!\n\n");
+			break;
+		}
+		else
+		{
+			printf("알 수 없는 오류가 발생했어요\n\n");
+		}
+	}
 	return 0;
 }
