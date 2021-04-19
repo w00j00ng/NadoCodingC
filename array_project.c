a4948cda86f82975aef70aada43da1ca0df35d61
w00j00ng <w00j00ng351@gmail.com>
2021-04-19 23:28:28 +0900

    Modify Array 1

--- array_project.c
+++ array_project.c
@@ -19,6 +19,7 @@ int main(void)
 		do {
 			cntShowBottle = rand() % 2 + 2; // 보여줄 병 개수 (0 ~ 1 + 2 -> 2 or 3)
 		} while (cntShowBottle == prevCntShowBottle);
+		prevCntShowBottle = cntShowBottle;
 		int isIncluded = 0; // 보여줄 병 중에 발모제가 포함되었는지 여부 (1: 포함, 0: 없음)
 		printf(" > %d번째 시도: ", i);
 
