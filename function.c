72bb802232c7e4137493960d0a657597866c939c
w00j00ng <w00j00ng351@gmail.com>
2021-04-19 18:52:50 +0900

    Start Calculator

--- function.c
+++ function.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 int main(void)
 {
+	// function
+	// 계산기
+	int num = 2;
+	printf("num은 %d입니다\n", num); // 2
+
+	// 2 + 3
+	num = num + 3;
+	printf("num은 %d입니다\n", num); // 5
+
+	// 5 - 1
+	num -= 1; // num = num - 1;
+	printf("num은 %d입니다\n", num); // 4
+
+	// 4 X 3은?
+	num *= 3;
+	printf("num은 %d입니다\n", num); // 12
+
+	// 12 / 6은?
+	num /= 6;
+	printf("num은 %d입니다\n", num); // 2
+
 	return 0;
 }
