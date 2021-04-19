63701a19affc03a5f91d74ad2e6f6dc5f3142774
w00j00ng <w00j00ng351@gmail.com>
2021-04-19 19:51:55 +0900

    Function 1

--- function_project.c
+++ function_project.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
+#include <time.h>
+
+int getRandomNumber(int level);
+void showQuestion(int level, int num1, int num2);
+void success();
+void fail();
 
 int main(void)
 {
+	// 문이 5개가 있고, 각 문마다 점점 어려운 수식 퀴즈가 출제됨 (랜덤)
+	// 맞히면 통과, 틀리면 실패
+
+	srand(time(NULL));
+	int count = 0; // 맞힌 문제 개수
+	for (int i = 1; i < 6; i++)
+	{
+		// x * y = ?
+		int num1 = getRandomNumber(i);
+		int num2 = getRandomNumber(i);
+		showQuestion(i, num1, num2);
+
+		int answer = -1;
+		scanf_s("%d", &answer);
+		if (answer == -1)
+		{
+			printf("프로그램을 종료합니다.\n");
+			exit(0); // 프로그램을 끝냄
+		}
+		else if (answer == num1 * num2)
+		{
+			// 성공
+			success();
+			count++;
+		}
+		else
+		{
+			// 실패
+			fail();
+		}
+	}
+
+	printf("\n\n당신은 5개의 비밀번호 중 %d개를 맞췄습니다.\n", count);
+
 	return 0;
 }
+
+int getRandomNumber(int level)
+{
+	return rand() % (level * 7) + 1;
+}
+
+void showQuestion(int level, int num1, int num2)
+{
+	printf("\n\n\n####### %d번 째 비밀번호.######\n", level);
+	printf("\n\t%d X %d 는? \n\n", num1, num2);
+	printf("#################################\n");
+	printf("\n비밀번호를 입력하세요 (종료: -1)\n");
+}
+
+void success()
+{
+	printf("\n >> Good ! 정답입니다.\n");
+}
+
+void fail()
+{
+	printf("\n >> Wrong ! 틀렸습니다..\n");
+}
+
