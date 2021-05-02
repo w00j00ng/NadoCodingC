b74f14b34129bd0dc6ed5f85bb8e45f37e7f5270
w00j00ng <w00j00ng351@gmail.com>
2021-05-02 17:49:49 +0900

    Add fgets

--- file.c
+++ file.c
@@ -11,15 +11,29 @@ int main(void)
 	
 	// fputs, fgets 한 쌍
 	char line[MAX]; // char line[10000]
-	FILE * file = fopen("c:\\projects\\nadoc\\test1.txt", "wb"); // r 읽기 전용, w 쓰기 전용, a 덧붙임, b 바이너리, t 텍스트
+	// 파일에 쓰기
+	// FILE * file = fopen("c:\\projects\\nadoc\\test1.txt", "wb"); // r 읽기 전용, w 쓰기 전용, a 덧붙임, b 바이너리, t 텍스트
+	// if (file == NULL)
+	// {
+		// printf("파일 열기 실패\n");
+		// return 1;
+	// }
+	
+	// fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
+	// fputs("잘 적히는지 확인해주세요\n", file);
+	
+	// 파일 읽기
+	FILE * file = fopen("c:\\projects\\nadoc\\test1.txt", "rb");
 	if (file == NULL)
 	{
 		printf("파일 열기 실패\n");
 		return 1;
 	}
 	
-	fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
-	fputs("잘 적히는지 확인해주세요\n", file);
+	while(fgets(line, MAX, file) != NULL)
+	{
+		printf("%s", line);
+	}
 	
 	// 파일을 열고 나서 닫지 않은 상태에서 어떤 프로그램에 문제가 생기면?
 	// 데이터 손실 발생 가능! 그래서 항상 파일은 닫아주는 습관을 들여주세요
