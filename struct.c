ddc550e6139e523c34c7c6338a2b1321c6d683f4
w00j00ng <w00j00ng351@gmail.com>
2021-05-02 16:51:46 +0900

    Struct 3

--- struct.c
+++ struct.c
@@ -9,6 +9,16 @@ struct GameInfo {
 	struct GameInfo * friendGame; // 연관 업체 게임
 };
 
+typedef struct {
+	char * name;
+	int year;
+	int price;
+	char * company;
+	
+	struct GameInfo * friendGame; // 연관 업체 게임
+} GAME_INFO;
+
+
 int main(void)
 {
 	// [게임 출시]
@@ -94,6 +104,23 @@ int main(void)
 	printf("   발매년도  : %d\n", gameInfo1.friendGame->year);
 	printf("   가격      : %d\n", gameInfo1.friendGame->price);
 	printf("   제작사    : %s\n", gameInfo1.friendGame->company);
+	
+	// typedef
+	// 자료형에 별명 지정
+	typedef int 정수;
+	typedef float 실수;
+	정수 정수변수 = 3; // int i = 3;
+	실수 실수변수 = 3.23f; // float f = 3.23f;
+	printf("정수변수: %d, 실수변수: %.2f\n\n", 정수변수, 실수변수);
+	
+	typedef struct GameInfo 게임정보;
+	게임정보 game1;
+	game1.name = "한글 게임";
+	game1.year = 2015;
+	
+	GAME_INFO game2;
+	game2.name = "한글 게임2";
+	game2.year = 2014;
 	
 	return 0;
 }
