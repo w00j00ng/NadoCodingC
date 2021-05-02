8d283e90a6ff85510feed6921cc8d85019853e14
w00j00ng <w00j00ng351@gmail.com>
2021-05-02 18:29:59 +0900

    Project file Attempt 1

--- file_project.c
+++ file_project.c
@@ -38,5 +38,46 @@ int main(void)
 		i++;
 	}
 	
+	// 비밀번호: 나도코딩 skehzheld
+	printf("\n\n === 비밀번호 확인 중 ... ===\n=n");
+	if (strcmp(password, "skehzheld") == 0) // 비밀번호 일치
+	{
+		printf(" === 비밀번호 확인 완료 === \n\n");
+		char * fileName = "c:\\projects\\nadoc\\secretdiary.txt";
+		FILE * file = fopen(filename, "a+b"); // 파일이 없으면 생성, 파일이 있으면 append를 한다. (뒤에서부터 내용을 추가한다)
+		if (file == NULL)
+		{
+			printf("파일 열기 실패\n");
+			return 1;
+		}
+		
+		while (fgets(line, MAX, file) != NULL)
+		{
+			printf("%s", line);
+		}
+		
+		pirntf("\n\n 내용을 계속 작성하세요 ! 종료하시려면 EXIT를 입력하세요\n\n");
+		
+		while (1)
+		{
+			// 한 문장 씩 저장
+			scanf("%[^\n]", contents); // 줄바꿈이 나오기 전까지 모든 문자열을 다 집어넣어라
+			getchar(); // Enter 입력 (\n) Flush 처리
+			
+			if (strcmp(contents, "EXIT") == 0)
+			{
+				printf("비밀일기 입력을 종료합니다\n\n");
+				break;
+			}
+			fputs(contents, file);
+			fputs("\n", file); // Enter를 위에서 무시 처리 하였으므로 임의로 추가
+		}
+		fclose();
+	}
+	else // 비밀번호가 틀린 경우
+	{
+		printf(" === 비밀번호가 틀렸어요 === \n\n");
+	}
+	
 	return 0;
 }
